57ba23fb6690ef643bc0a9401f79411c4f8ab37f
sssun <98_0509@naver.com>
2024-03-01 18:23:35 +0900

    CompletableFuture

--- src/main/kotlin/future/Future.kt
+++ src/main/kotlin/future/Future.kt
@@ -2,6 +2,7 @@ package future
 
 import java.util.concurrent.Callable
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 fun sum(a: Int, b: Int) = a + b
 
@@ -12,7 +13,8 @@ fun main() {
     })
 
     println("계산 시작")
-    val futureResult = future.get() // 비동기 작업의 결과를 기다린다.
+    val futureResult = future.get() // 비동기 작업의 결과를 기다린다. 블로킹 방식의 비동기 작업
+    // val futureResult = future.get(1000L, TimeUnit.MILLISECONDS)
     println(futureResult)
     println("계산 종료")
 }
